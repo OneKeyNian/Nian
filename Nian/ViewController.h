ed6bdfa600e70a0129ca0416a9e8252cc0d544f8
ZH <zhouhong>
2015-07-14 10:20:07 +0800

    Initial Commit

---
+++ Nian/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Nian
+//
+//  Created by 周宏 on 15/7/14.
+//  Copyright (c) 2015年 周宏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Nian/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Nian
+//
+//  Created by 周宏 on 15/7/14.
+//  Copyright (c) 2015年 周宏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
